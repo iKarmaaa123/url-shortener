0bc19c574fc7e525f700c0510a32eaafeb75dfa7
iKarmaa123 <waael@hotmail.co.uk>
2026-02-14 22:10:34 +0000

    feat: added ecs fargate service class for ecs cluster construct

--- lib/constructs/ecs-cluster-construct.ts
+++ lib/constructs/ecs-cluster-construct.ts
@@ -1,23 +1,34 @@
 import * as cdk from 'aws-cdk-lib/core';
-import { IVpc } from "aws-cdk-lib/aws-ec2"
+import { IVpc, SubnetSelection, ISecurityGroup } from "aws-cdk-lib/aws-ec2"
 import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as iam from "aws-cdk-lib/aws-iam";
 import { IRole } from "aws-cdk-lib/aws-iam"
 import { Construct } from 'constructs';
 
 export interface ecsConstructProps extends cdk.StackProps {
-    clusterName: string
+    clusterName?: string
     vpc: IVpc
-    enableFargateCapacityProviders: boolean
-    memoryLimitMiB: number
-    cpu: number
-    executionRole: IRole
-    taskRole: IRole
+    vpcSubnets: SubnetSelection
+    securityGroups: ISecurityGroup[]
+    enableFargateCapacityProviders?: boolean
+    executionRole?: IRole
+    taskRole?: IRole
+    serviceName?: string
+    desiredCount?: number
     image: ecs.ContainerImage
+    environment?: {
+      TABLE_NAME?: string
+    }
 }
 
 export class ecsConstruct extends Construct {
   constructor(scope: Construct, id: string, props: ecsConstructProps) {
     super(scope, id);
+
+    const executionRole = new iam.Role(this, "executionRole", {
+      assumedBy: new iam.ServicePrincipal("ecs.amazonaws.com"),
+      managedPolicies: [iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AmazonECSTaskExecutionRolePolicy")]
+    })
 
 
     const cluster = new ecs.Cluster(this, "Cluster", {
@@ -27,19 +38,25 @@ export class ecsConstruct extends Construct {
     })
 
     const taskDefinition = new ecs.FargateTaskDefinition(this, "taskDefinition", {
-      memoryLimitMiB: props.memoryLimitMiB,
-      cpu: props.cpu,
-      executionRole: props.executionRole,
+      memoryLimitMiB: 512,
+      cpu: 256,
+      executionRole: executionRole,
       taskRole: props.taskRole
     })
 
     taskDefinition.addContainer("web", {
-      image: props.image
+      image: props.image,
+      environment: props.environment
     })
 
     const fargateService = new ecs.FargateService(this, "fargateService", {
+      serviceName: props.serviceName,
+      desiredCount: props.desiredCount,
       cluster,
       taskDefinition,
+      vpcSubnets: props.vpcSubnets,
+      securityGroups: props.securityGroups,
+      assignPublicIp: true
     })
   }
 }
