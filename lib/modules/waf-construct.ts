517f8a9b05c1fd0a1fcccf28e8ab73489cbab0c9
iKarmaa123 <waael@hotmail.co.uk>
2026-03-12 00:26:21 +0000

    feat: created waf construct

---
+++ lib/modules/waf-construct.ts
@@ -0,0 +1,50 @@
+import * as waf from "aws-cdk-lib/aws-wafv2";
+import { ILoadBalancerRef } from "aws-cdk-lib/aws-elasticloadbalancing";
+import { IGraphQLApiRef } from "aws-cdk-lib/aws-appsync";
+import { Construct } from "constructs";
+
+interface WafConstructProps {
+  block?: waf.CfnWebACL.BlockActionProperty;
+  scope: string;
+  name?: string;
+  visibilityConfig: waf.CfnWebACL.VisibilityConfigProperty;
+  ruleName: string;
+  priority: number;
+  action: waf.CfnWebACL.RuleActionProperty;
+  statement: waf.CfnWebACL.StatementProperty;
+  resourceArn: string;
+}
+
+export class WafContruct extends Construct {
+  constructor(scope: Construct, id: string, props: WafConstructProps) {
+    super(scope, id);
+
+    const defaultActionProperty: waf.CfnWebACL.DefaultActionProperty = {
+      block: props.block,
+    };
+
+    const wafRules: waf.CfnWebACL.RuleProperty[] = [
+      {
+        name: props.ruleName,
+        priority: props.priority,
+        action: props.action,
+        statement: props.statement,
+        visibilityConfig: props.visibilityConfig,
+      },
+    ];
+
+    const customWaf = new waf.CfnWebACL(this, "waf", {
+      defaultAction: defaultActionProperty,
+      scope: props.scope,
+      name: props.name,
+      visibilityConfig: props.visibilityConfig,
+      rules: wafRules,
+    });
+
+    const wafAclAssociation: waf.CfnWebACLAssociation =
+      new waf.CfnWebACLAssociation(this, "wafAclAssociation", {
+        webAclArn: customWaf.attrArn,
+        resourceArn: props.resourceArn,
+      });
+  }
+}
