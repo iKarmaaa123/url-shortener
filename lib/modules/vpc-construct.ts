0408db385fc5e31234256beb45c48d245eaa464c
iKarmaa123 <waael@hotmail.co.uk>
2026-03-17 02:49:41 +0000

    chore: updated vpc module

--- lib/modules/vpc-construct.ts
+++ lib/modules/vpc-construct.ts
@@ -3,17 +3,22 @@ import * as ec2 from "aws-cdk-lib/aws-ec2";
 
 export interface vpcConstructProps {
   vpcName?: string;
-  ipAddresses: ec2.IIpAddresses;
-  availabilityZones: string[];
-  subnetConfiguration: ec2.SubnetConfiguration[];
+  ipAddresses?: ec2.IIpAddresses;
+  availabilityZones?: string[];
+  subnetConfiguration?: ec2.SubnetConfiguration[];
   createInternetGateway?: boolean;
   ecsSecurityGroupName?: string;
   albSecurityGroupName?: string;
   natGateways?: number;
   allowAllOutbound?: boolean;
   mapPublicIpOnLaunch?: boolean;
-  serviceRegion: string;
-  ipAddressType: ec2.VpcEndpointIpAddressType;
+  serviceRegion?: string;
+  vpcInterfaceEndpointServiceECR?: ec2.IInterfaceVpcEndpointService;
+  vpcInterfaceEndpointServiceCloudWatch?: ec2.IInterfaceVpcEndpointService;
+  vpcGatewayEndpointServiceDynamodb?: ec2.IGatewayVpcEndpointService;
+  ipAddressType?: ec2.VpcEndpointIpAddressType;
+  privateDnsEnabled?: boolean;
+  privateDnsOnlyForInboundResolverEndpoint?: ec2.VpcEndpointPrivateDnsOnlyForInboundResolverEndpoint;
 }
 
 export class VpcConstruct extends Construct {
@@ -55,29 +60,34 @@ export class VpcConstruct extends Construct {
       allowAllOutbound: props.allowAllOutbound,
     });
 
-    const vpcInterfaceForECR = new ec2.InterfaceVpcEndpoint(
-      this,
-      "vpcInterfaceEndpoint",
-      {
-        service: ec2.InterfaceVpcEndpointAwsService.ECR,
-        vpc: this.vpc,
+    if (props.vpcInterfaceEndpointServiceECR) {
+      this.vpc.addInterfaceEndpoint("ecrVpcInterfaceEndpoint", {
+        service: props.vpcInterfaceEndpointServiceECR,
         subnets: this.privateSubnets,
         serviceRegion: props.serviceRegion,
         ipAddressType: props.ipAddressType,
-      },
-    );
+        privateDnsEnabled: props.privateDnsEnabled,
+        privateDnsOnlyForInboundResolverEndpoint: props.privateDnsOnlyForInboundResolverEndpoint
+      })
+    }
 
-    const vpcInterfaceForDynamoDB = new ec2.InterfaceVpcEndpoint(
-      this,
-      "vpcInterfaceEndpoint",
-      {
-        service: ec2.InterfaceVpcEndpointAwsService.DYNAMODB,
-        vpc: this.vpc,
+    if (props.vpcInterfaceEndpointServiceCloudWatch) {
+      this.vpc.addInterfaceEndpoint("cloudWatchVpcInterfaceEndpoint", {
+        service: props.vpcInterfaceEndpointServiceCloudWatch,
         subnets: this.privateSubnets,
         serviceRegion: props.serviceRegion,
         ipAddressType: props.ipAddressType,
-      },
-    );
+        privateDnsEnabled: props.privateDnsEnabled,
+        privateDnsOnlyForInboundResolverEndpoint: props.privateDnsOnlyForInboundResolverEndpoint
+      })
+    }
+
+    if (props.vpcGatewayEndpointServiceDynamodb) {
+      this.vpc.addGatewayEndpoint("dynmaodbVpcGatewayEndpoint", {
+        service: props.vpcGatewayEndpointServiceDynamodb,
+        subnets: [this.privateSubnets],        
+      })
+    }
 
     this.ecsSecurityGroup.addIngressRule(this.albSecurityGroup, ec2.Port.tcp(8080));
     this.ecsSecurityGroup.addIngressRule(this.albSecurityGroup, ec2.Port.tcp(443));
