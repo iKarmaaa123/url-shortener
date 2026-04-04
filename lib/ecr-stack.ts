f0de203270d6987b24af06e0d3804f8318ea9fbd
iKarmaa123 <waael@hotmail.co.uk>
2026-04-04 15:09:11 +0100

    feat: created ecr stack

---
+++ lib/ecr-stack.ts
@@ -0,0 +1,24 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { EcrConstruct } from "./modules/ecr-construct";
+
+export class EcrStack extends cdk.Stack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    new EcrConstruct(this, "ecrApiRepo", {
+      repositoryName: "ecrApiRepository",
+      emptyOnDelete: true,
+    });
+
+    new EcrConstruct(this, "ecrWorkerRepo", {
+      repositoryName: "ecrApiRepository",
+      emptyOnDelete: true,
+    });
+
+    new EcrConstruct(this, "ecrDashbaordRepo", {
+      repositoryName: "ecrApiRepository",
+      emptyOnDelete: true,
+    });
+  }
+}
