677a43e9ce9c3d138264deb60ae1ad4c0b798a37
iKarmaa123 <waael@hotmail.co.uk>
2026-04-04 15:21:27 +0100

    feat: created new ecr module

---
+++ lib/modules/ecr-construct.ts
@@ -0,0 +1,19 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ecr from "aws-cdk-lib/aws-ecr";
+
+export interface EcrConstructProps {
+  repositoryName: string;
+  emptyOnDelete: boolean;
+}
+
+export class EcrConstruct extends Construct {
+  constructor(scope: Construct, id: string, props: EcrConstructProps) {
+    super(scope, id);
+
+    new ecr.Repository(this, "apiEcrRepo", {
+      repositoryName: props.repositoryName,
+      emptyOnDelete: props.emptyOnDelete,
+    });
+  }
+}
