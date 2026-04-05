69ce63efc03dbceaab0cec85508e86f1cf48b56a
iKarmaa123 <waael@hotmail.co.uk>
2026-04-05 15:41:04 +0100

    fix: fixed formatting in module

--- lib/modules/sqs-construct.ts
+++ lib/modules/sqs-construct.ts
@@ -3,21 +3,21 @@ import * as sqs from "aws-cdk-lib/aws-sqs"
 import { Construct }  from "constructs"
 
 export interface SqsConstructProps {
-    queueName: string
-    receiveMessageWaitTime: cdk.Duration
-    visibilityTimeout: cdk.Duration
+  queueName: string
+  receiveMessageWaitTime: cdk.Duration
+  visibilityTimeout: cdk.Duration
 }
 
 export class SqsConstruct extends Construct {
+  public readonly sqsQueue: cdk.aws_sqs.Queue
 
-    public readonly sqsQueue: cdk.aws_sqs.Queue
-    constructor(scope: Construct, id: string, props: SqsConstructProps) {
-        super(scope, id)
+  constructor(scope: Construct, id: string, props: SqsConstructProps) {
+    super(scope, id)
 
-        this.sqsQueue = new sqs.Queue(this, "sqsQueue", {
-            queueName: props.queueName,
-            receiveMessageWaitTime: props.receiveMessageWaitTime,
-            visibilityTimeout: props.visibilityTimeout,
-        })
-    }
+    this.sqsQueue = new sqs.Queue(this, "sqsQueue", {
+      queueName: props.queueName,
+      receiveMessageWaitTime: props.receiveMessageWaitTime,
+      visibilityTimeout: props.visibilityTimeout,
+    })
+  }
 }
