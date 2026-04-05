d1cc7a48d37021bd8d46bdf9742c4690cae2e722
iKarmaa123 <waael@hotmail.co.uk>
2026-04-05 16:04:41 +0100

    chore: updated app file with new ecr stack

--- bin/ecs-project-v2.ts
+++ bin/ecs-project-v2.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib/core";
 import { EcsStack } from "../lib/ecs-stack";
+import { EcrStack } from "../lib/ecr-stack"
 
 const app = new cdk.App();
 new EcsStack(app, "ecsStack");
+new EcrStack(app, "ecrStack");
