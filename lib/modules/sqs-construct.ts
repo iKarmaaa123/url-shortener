58f97281d5f8f0a5e7bce79a630300060ce1cfba
iKarmaa123 <waael@hotmail.co.uk>
2026-04-07 21:58:03 +0100

    chore: added semi colons to all properities part of SqsConstructProps and for all imports

--- lib/modules/sqs-construct.ts
+++ lib/modules/sqs-construct.ts
@@ -1,23 +1,23 @@
-import * as cdk from "aws-cdk-lib"
-import * as sqs from "aws-cdk-lib/aws-sqs"
-import { Construct }  from "constructs"
+import * as cdk from "aws-cdk-lib";
+import * as sqs from "aws-cdk-lib/aws-sqs";
+import { Construct } from "constructs";
 
 export interface SqsConstructProps {
-  queueName: string
-  receiveMessageWaitTime: cdk.Duration
-  visibilityTimeout: cdk.Duration
+  queueName: string;
+  receiveMessageWaitTime: cdk.Duration;
+  visibilityTimeout: cdk.Duration;
 }
 
 export class SqsConstruct extends Construct {
-  public readonly sqsQueue: cdk.aws_sqs.Queue
+  public readonly sqsQueue: cdk.aws_sqs.Queue;
 
   constructor(scope: Construct, id: string, props: SqsConstructProps) {
-    super(scope, id)
+    super(scope, id);
 
     this.sqsQueue = new sqs.Queue(this, "sqsQueue", {
       queueName: props.queueName,
       receiveMessageWaitTime: props.receiveMessageWaitTime,
       visibilityTimeout: props.visibilityTimeout,
-    })
+    });
   }
 }
