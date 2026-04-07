fc63c19943c8d0376634f05ad47d466efab236da
iKarmaa123 <waael@hotmail.co.uk>
2026-04-07 21:51:52 +0100

    feat: created enum for engine and turned cacheSubnetGroupName into optional property from required property

--- lib/modules/elasticacheredis-construct.ts
+++ lib/modules/elasticacheredis-construct.ts
@@ -1,31 +1,35 @@
 import { CfnCacheCluster, CfnSubnetGroup } from "aws-cdk-lib/aws-elasticache";
-import { IVpc, SubnetType } from "aws-cdk-lib/aws-ec2"
 import { Construct } from "constructs";
 
+export enum Engine {
+  redis = "redis",
+  memcached = "memcached",
+  valkey = "valkey",
+}
+
 export interface ElastiCacheRedisProps {
-    clusterName?: string;
-    cacheNodeType: string;
-    engine: string;
-    numCacheNodes: number;
-    autoMinorVersionUpgrade: boolean;
-    networkType?: string;
-    cacheSubnetGroupName: string;
-    vpcSecurityGroupIds: string[];
+  clusterName?: string;
+  cacheNodeType: string;
+  engine: Engine;
+  autoMinorVersionUpgrade: boolean;
+  networkType?: string;
+  cacheSubnetGroupName?: string;
+  vpcSecurityGroupIds: string[];
 }
 
 export class ElastiCacheRedis extends Construct {
   constructor(scope: Construct, id: string, props: ElastiCacheRedisProps) {
-    super(scope, id)
+    super(scope, id);
 
     new CfnCacheCluster(this, "elasticCacheCluster", {
       clusterName: props.clusterName,
       cacheNodeType: props.cacheNodeType,
       engine: props.engine,
-      numCacheNodes: props.numCacheNodes,
+      numCacheNodes: props.engine === Engine.redis ? 1 : 0,
       autoMinorVersionUpgrade: props.autoMinorVersionUpgrade,
       networkType: props.networkType,
       cacheSubnetGroupName: props.cacheSubnetGroupName,
       vpcSecurityGroupIds: props.vpcSecurityGroupIds,
-    })
+    });
   }
 }
